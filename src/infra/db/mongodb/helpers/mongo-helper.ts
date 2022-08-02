951f49eade3c63c05c9dbaacd25581ca6dbf8fb3
Andre Teixeira <andreteixeira628@gmail.com>
2022-08-02 17:18:28 -0300

    refactor: move mapping to MongoHelper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -12,5 +12,9 @@ export const MongoHelper = {
 
   getCollection (name: string): Collection {
     return this.client.db().collection(name)
+  },
+
+  map: (collection: any, collectionWithoutId: any): any => {
+    return { id: collection.insertedId.toString(), ...collectionWithoutId }
   }
 }
